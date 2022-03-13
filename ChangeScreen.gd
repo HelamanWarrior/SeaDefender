eb0e6d47fc4e1d1656013e49f9e631860badefcf
PlugWorld <40321938+HelamanWarrior@users.noreply.github.com>
2022-03-12 19:26:09 -0700

    Title screen

--- ChangeScreen.gd
+++ ChangeScreen.gd
@@ -18,30 +18,20 @@ func _ready():
 
 
 func _on_Play_button_down():
-	print("Playing:")
 	get_tree().change_scene("Prototype.tscn")
-	pass # Replace with function body.
 
 
 func _on_Settings_button_down():
-	print("Going to Controls Menu:")
 	get_tree().change_scene("ControlsMenu.tscn")
-	pass # Replace with function body.
 
 
 func _on_Exit_button_down():
-	print("Exiting:")
 	get_tree().quit()
-	pass # Replace with function body.
 
 
 func _on_Back_button_down():
-	print("Going To Main Menu:")
 	get_tree().change_scene("Menu.tscn")
-	pass # Replace with function body.
 
 
 func _on_Pause_button_down():
-	print("Pausing To Main Menu:")
 	get_tree().change_scene("Menu.tscn")
-	pass # Replace with function body.
